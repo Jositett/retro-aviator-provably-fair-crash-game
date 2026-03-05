aec3013a02606096f89f1d624ae0b606bbe21dae
Vibesdk <vibesdk-bot@cloudflare.com>
2026-03-05 11:17:46 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add crypto-js@^4.2.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
